448d7f0122a92a2ee276eda0425990a6ee4d78e3
Johannes Mario Meissner <jmariomeissner@gmail.com>
2023-10-29 12:24:36 +0900

    Fix vercel analytics again?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mariomeissner.dev/", // replace this with your deployed domain
+  output: "server",
   integrations: [
     tailwind({
       config: {
