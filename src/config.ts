57f8eee69a71f8438361d3a840f476dca91264a6
Johannes Mario Meissner <jmariomeissner@gmail.com>
2026-05-15 10:52:43 +0900

    Show ten posts per page

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Johannes Mario Meissner",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 5,
+  postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
